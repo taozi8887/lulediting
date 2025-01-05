9dac301fd7a9dc7a445a480ceec411e1320b77ae
Edward Luo <321joemama123@gmail.com>
2025-01-04 19:40:15 -0700

    404

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-document.getElementById("year").innerHTML = new Date().getFullYear();
+
 // Function to toggle the expanded/collapsed state of the navbar
 function toggleMenu() {
     var navbarItems = document.querySelector('.navbar');
     navbarItems.classList.toggle('shrunk');
     document.querySelector('.hamburger-container').classList.toggle('open');
 }
+
+window.onload = function() {
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+};
